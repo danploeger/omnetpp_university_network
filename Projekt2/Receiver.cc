6f72761afc73b72bfe29b375f35ba5250424c775
Daniel <dp.service@posteo.de>
2016-05-01 17:11:08 +0200

    Code prettify

--- Projekt2/Receiver.cc
+++ Projekt2/Receiver.cc
@@ -1,12 +1,10 @@
 
-
-
 #include "Receiver.h"
 #include <stdio.h>
 #include <string.h>
 #include <omnetpp.h>
 
-class receiver : public cSimpleModule{
+class receiver: public cSimpleModule {
 
 public:
     cMessage *tictocmsg;
@@ -16,45 +14,36 @@ public:
     cLongHistogram hopCountStats;
     cOutVector hopCountVector;
 
-protected: //zwei funktionen:
+protected:
+    //zwei funktionen:
     //wird einmal aufgerufen
     virtual void initialize();
     //wird jedes mal aufgerufen, wenn eine message ankommt
     //event UND normale message
     virtual void handleMessage(cMessage *msg);
-
-
-
 };
 
 Define_Module(receiver);
 
-void receiver :: initialize(){
-    interArrivalTime=0;
-  //ob initiator oder nicht, wird in .ned/file entschieden
-    if(strcmp("S",getName())==0){
+void receiver::initialize() {
+    interArrivalTime = 0;
+    //ob initiator oder nicht, wird in .ned/file entschieden
+    if (strcmp("S", getName()) == 0) {
 
-        EV<<"Scheduling first selfMessage at 5s \n";
+        EV << "Scheduling first selfMessage at 5s \n";
         cMessage *msg = new cMessage("tictocMsg");
         send(msg, "out");
     }
+}
+;
 
-
-
-
-
-};
-
-void receiver :: handleMessage(cMessage *msg){
-
-    interArrivalTime=simTime()-receivedTimeRecord;
+void receiver::handleMessage(cMessage *msg) {
+    interArrivalTime = simTime() - receivedTimeRecord;
     receivedTimeRecord = simTime();
 
     hopCountStats.collect(interArrivalTime);
     hopCountVector.record(interArrivalTime);
 
-   EV<<interArrivalTime;
-
-    }
-
+    EV << interArrivalTime;
+}
 
