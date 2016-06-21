0dbb471ae840f72c58ab1c11c9ed3c0a9bd33fd8
Daniel <dp.service@posteo.de>
2016-06-21 16:20:31 +0200

    VoIPApp fix

---
+++ INET_Exercises/VoIPApp.h
@@ -0,0 +1,23 @@
+/*
+ * VoIPApp.h
+ *
+ *  Created on: 19.06.2016
+ *      Author: Daniel Plöger
+ */
+
+#ifndef VOIPAPP_H_
+#define VOIPAPP_H_
+
+#include <UDPBasicApp.h>
+
+class VoIPApp : public UDPBasicApp
+{
+protected:
+    virtual void sendPacket();
+    virtual void processPacket(cPacket *msg);
+public:
+    VoIPApp();
+    ~VoIPApp();
+};
+
+#endif /* VOIPAPP_H_ */
