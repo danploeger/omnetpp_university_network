740175ab7d25f2a5f94ba4161064063f707e3b18
Daniel <dp.service@posteo.de>
2016-06-21 16:20:31 +0200

    VoIPApp added

---
+++ INET_Exercises/VoIPApp.cc
@@ -0,0 +1,35 @@
+/*
+ * VoIPApp.cc
+ *
+ *  Created on: 19.06.2016
+ *      Author: Daniel
+ */
+
+#include <VoIPApp.h>
+
+VoIPApp::VoIPApp() {
+    selfMsg = NULL;
+}
+
+VoIPApp::~VoIPApp() {
+    cancelAndDelete(selfMsg);
+}
+
+Define_Module(VoIPApp);
+
+//simsignal_t VoIPApp::sentPkSignal = registerSignal("sentPk");
+//simsignal_t VoIPApp::rcvdPkSignal = registerSignal("rcvdPk");
+
+void VoIPApp::sendPacket()
+{
+    UDPBasicApp::sendPacket();
+    recordScalar("packet-departure-time", simTime());
+}
+
+void VoIPApp::processPacket(cPacket *pk)
+{
+    UDPBasicApp::processPacket(cPacket *pk);
+    recordScalar("packet-arrival-time", simTime());
+    recordScalar("arriving-packet-number", UDPBasicApp::numReceived);
+}
+
---
+++ INET_Exercises/VoIPApp.h
@@ -0,0 +1,20 @@
+/*
+ * VoIPApp.h
+ *
+ *  Created on: 19.06.2016
+ *      Author: Daniel Plöger
+ */
+
+#ifndef VOIPAPP_H_
+#define VOIPAPP_H_
+
+#include <UDPBasicApp.h>
+
+class VoIPApp : public UDPBasicApp
+{
+public:
+    VoIPApp();
+    ~VoIPApp();
+};
+
+#endif /* VOIPAPP_H_ */
